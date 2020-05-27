74e2ceee11570a23405394ea476215b0e21fd6c7
Matheus <matheus_diassousa@hotmail.com>
2020-05-27 09:58:00 -0300

    ELD_

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
-          title: Text('I am Rich'),
+          title: Center(child: Text('I am Rich', textAlign: TextAlign.center),),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://feninjer.com.br/wp-content/uploads/2019/07/Diamond-768x432.jpeg'),
+          ),
         ),
       ),
     ),
